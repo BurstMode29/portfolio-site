03e3756086c3b1fb33b3f5bff5f6e6e2a0fecc93
Rafeeq Weideman <r.w.weideman.rafeeq@gmail.com>
2023-01-12 14:43:46 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './style.css';
 
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,12 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../style.css';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+
+
 
 
 
 const Projects = () => {
+
+    const [read, setRead] = useState([]);
+
+    const ReadData = async () => {
+        await getDocs(collection(db, "projects"))
+            .then((querySnapshot) => {
+                const newData = querySnapshot.docs
+                    .map((doc) => ({ ...doc.data(), id: doc.id }));
+                setRead(newData);
+            })
+    }
+
+    useEffect(() => {
+        ReadData();
+    }, [])
+
+    const ProjectCard = () => {
+        return (
+            <div>
+                <tr >
+                    {
+                        read?.map((data, i) => (
+                            <td key={i}>
+                                <div className="projectName">{data.name}</div>
+                                <div className="projectDescription">{data.description}</div>
+                                <div className="projectType">{data.type}</div>
+                            </td>
+                        ))
+                    }
+                </tr>
+            </div>
+        )
+    };
+
     return (
-        <div>
+        <div className="projects">
             <div>Projects</div>
+            <ProjectCard />
         </div>
     )
 }
